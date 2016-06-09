d99ebe753ad3232316e93b199e236943f9aa4224
patricio <patt244@yahoo.com.ar>
2016-06-09 11:06:10 -0300

    elimino archivos generados en la compilacion

--- Webserver/Debug/Control/src/actuators.d
+++
@@ -1,8 +0,0 @@
-Control/src/actuators.o Control/src/actuators.d: \
- ../Control/src/actuators.c \
- /home/pato/Embebidos/TpFinal/Workspaces/TpFinal/Webserver/Control/inc/include.h \
- /home/pato/Embebidos/TpFinal/Workspaces/TpFinal/Webserver/Control/inc/actuators.h
-
-/home/pato/Embebidos/TpFinal/Workspaces/TpFinal/Webserver/Control/inc/include.h:
-
-/home/pato/Embebidos/TpFinal/Workspaces/TpFinal/Webserver/Control/inc/actuators.h:
--- Webserver/Debug/Control/src/sensors.d
+++
@@ -1,7 +0,0 @@
-Control/src/sensors.o Control/src/sensors.d: ../Control/src/sensors.c \
- /home/pato/Embebidos/TpFinal/Workspaces/TpFinal/Webserver/Control/inc/sensors.h \
- /home/pato/Embebidos/TpFinal/Workspaces/TpFinal/Webserver/HAL/Chip/inc/lpc_types.h
-
-/home/pato/Embebidos/TpFinal/Workspaces/TpFinal/Webserver/Control/inc/sensors.h:
-
-/home/pato/Embebidos/TpFinal/Workspaces/TpFinal/Webserver/HAL/Chip/inc/lpc_types.h:
